5b786447372344052dfbf0eb0583ec4d64868d62
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-25 16:19:14 +0530

    panda is created

---
+++ anju.py
@@ -0,0 +1,20 @@
+ class Animal:
+     def eat(self):
+         print("I can Eat")
+
+     def sleep(self):
+         print("I can Sleep")
+
+     def sound(self):
+         print("I can Sound")
+
+     def run(self):
+         print("I can Run")
+
+     def fly(self):
+         print("I can Fly")
+
+    def lazy(self):
+        print("I am lazy")
+
+
